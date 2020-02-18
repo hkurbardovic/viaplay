7bdb30cf24b51c96e0acc1e32c8f4712c67c1adf
hkurbardovic <hkurbardovich@melontech.com>
2020-02-18 20:33:32 +0100

    no message

--- app/src/main/java/com/hkurbardovic/viaplay/di/modules/ActivityBindingModule.kt
+++ app/src/main/java/com/hkurbardovic/viaplay/di/modules/ActivityBindingModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
+@Suppress("UNUSED")
 abstract class ActivityBindingModule {
 
     @ActivityScoped
--- app/src/main/java/com/hkurbardovic/viaplay/di/modules/SectionsModule.kt
+++ app/src/main/java/com/hkurbardovic/viaplay/di/modules/SectionsModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
+@Suppress("UNUSED")
 internal abstract class SectionsModule {
 
     @FragmentScoped
